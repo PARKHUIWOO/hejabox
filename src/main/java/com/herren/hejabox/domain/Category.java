83a8c357d86c40c0d9c400cf812b0cea6116e83d
PARKHUIWOO <huiwo0@naver.com>
2019-11-24 21:11:13 +0900

    develop entity class

--- src/main/java/com/herren/hejabox/domain/Category.java
+++ src/main/java/com/herren/hejabox/domain/Category.java
@@ -25,7 +25,7 @@ public class Category {
             inverseJoinColumns = @JoinColumn(name = "item_id")) // Category랑 Items랑 일대다-다대일 관계로 매핑
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
@@ -33,4 +33,9 @@ public class Category {
     private List<Category> child = new ArrayList<>();
 
 
+    //== 연관관계 편의 메서드 ==//
+    public void addChildCategory(Category child) { //카테고리 셀프, 양방향이어야 됨. child를 집어넣으면 양쪽에 다 들어가야함
+        this.child.add(child); //
+        child.setParent(this); // 자식에게도 부모가 누구인지를 넣어줘야함
+    }
 }
--- src/main/java/com/herren/hejabox/domain/Order.java
+++ src/main/java/com/herren/hejabox/domain/Order.java
@@ -18,19 +18,48 @@ public class Order {
     @Column(name = "orders_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
+    // Order 저장하면 orderItems에 데이터를 넣어주고 저장하면, 얘도 같이 저장된다.
 
-    @OneToOne
+//    persist(orderItemA)
+//    persist(orderItemB)
+//    persist(orderItemC)
+//    persist(order)
+//
+//    엔티티당 각각 persist 호출해야하는데, cascade를 두게되면
+//
+//    persist(order) 얘만 두면됨.
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL) // cascade order를 저장할 때, delivery도 저장해줌.
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
+    // 카멜케이스가 언더바로 바뀜 -> order_date
     private LocalDateTime orderDate; // 주문시간
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+
+    //== 연관관계 메서드== // 양방향일 때 세팅하면 좋다.
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
 }
